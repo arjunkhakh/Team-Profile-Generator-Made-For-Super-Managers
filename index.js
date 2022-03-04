b8fd1e4e5d20a19cb6fbe09051c726c62ad4f86c
Arjun Singh <arjunkhakh@gmail.com>
2022-03-04 20:23:42 +0000

    started to work on the questions and fs and prompt commands

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 
+const inq = require("inquirer");
+
 const questions = [
     {
         type: "input",
-        message: "Hello and Welcome to this application on creating your Team Profile! Lets get started with your Manager. What is your managers name?",
+        message: "What is your Managers name?",
         name: "managerName",
     },
     {
@@ -24,4 +26,29 @@ const questions = [
         message: "What is your Managers Office Number?",
         name: "managerNumber",
     },
+    {
+        type: 'list',
+        name: 'list',
+        message: 'What Would You Like To Add Next?',
+        choices : [
+            'Engineer',
+            'Intern',
+            'No More Members'
+        ],
+        
+    }
 ]
+
+function writeToFile(filename, data) {
+    fs.writeFile(filename, generateMarkdown(data), function (err) {
+        if (err) throw err;
+        console.log("Saved!");
+      })
+
+      ;
+}
+
+inq.prompt(questions).then((answers) => {
+    writeToFile("index.html", answers);
+  }
+  )
